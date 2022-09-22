c172df190b5f8757c8f03ab5396e38e1b530752b
lmxlucy <56939921+lmxlucy@users.noreply.github.com>
2022-09-22 11:29:44 -0400

    matched query using regex

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -1,13 +1,17 @@
 package com.develogical;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class QueryProcessor {
 
     public String process(String query) {
-        if (query.toLowerCase().contains("lucy")) {
-            return "Lucy is a student at CMU.";
-        }
-        if (query.toLowerCase().contains("17313")) {
-            return "17313 is a course at CMU.";
+        
+        if (query.toLowerCase().contains("largest")) {
+            Pattern regex = Pattern.compile("which of the following numbers is the largest: (\\w+)");
+            Matcher matcher = regex.matcher(query);
+            return String.valueOf(Arrays.stream(matcher.group(1).split(",")).mapToInt(Integer::parseInt).max());
         }
         return "";
     }
